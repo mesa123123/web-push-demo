a5335d48a4ac3dfd1fc7269f417c11f35d79b53a
Peter Bowman <BOWMAP01@heiway.net>
2019-10-25 12:37:37 +1300

    Peters Copy First

--- frontend/index.js
+++ frontend/index.js
@@ -1,31 +1,37 @@
+console.log('JS is working!')
+
 const check = () => {
-  if (!("serviceWorker" in navigator)) {
-    throw new Error("No Service Worker support!");
-  }
-  if (!("PushManager" in window)) {
-    throw new Error("No Push API Support!");
-  }
-};
+    if (!('serviceWorker' in navigator)) {
+        throw new Error('Service Worker Not Supported!')
+    }
+    if (!('PushManager' in window)) {
+        throw new Error('No Push API Support!')
+    }
+}
 
 const registerServiceWorker = async () => {
-  const swRegistration = await navigator.serviceWorker.register("service.js");
-  return swRegistration;
-};
+    const swRegistration = await navigator.serviceWorker.register('service.js')
+    return swRegistration;
+}
 
 const requestNotificationPermission = async () => {
-  const permission = await window.Notification.requestPermission();
-  // value of permission can be 'granted', 'default', 'denied'
-  // granted: user has accepted the request
-  // default: user has dismissed the notification permission popup by clicking on x
-  // denied: user has denied the request.
-  if (permission !== "granted") {
-    throw new Error("Permission not granted for Notification");
-  }
-};
+    const permission = await window.Notification.requestPermission()
+    console.log(Notification.permission)
+    if(permission !== 'granted') {
+        throw new Error('Permission not granted for Notifications')
+    }
+}
+
+const showLocalNotification = (title, body, swRegistration) => {
+    const options = {
+        body
+    }
+    swRegistration.showNotification(title, options)
+}
 
 const main = async () => {
-  check();
-  const swRegistration = await registerServiceWorker();
-  const permission = await requestNotificationPermission();
-};
-// main(); we will not call main in the beginning.
+    check()
+    const swRegistration = await registerServiceWorker()
+    const permission = await requestNotificationPermission()
+    showLocalNotification('This is the title', 'this is the message', swRegistration)
+}
